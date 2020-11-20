1cc691309ed1bf645d18c660b26cb167728f82d1
Meredith Hansen <meredithghansen@gmail.com>
2020-11-19 19:12:40 -0800

    add user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// business logic
 function add(number1, number2) {
   return number1 + number2;
   }
@@ -14,7 +15,13 @@ function divide(number1, number2) {
   return number1 / number2;
   }
 
-  const number1 = parseInt(prompt("Enter a number:"));
-  const number2 = parseInt(prompt("Enter another number:"));
-  
-  alert(add(number1,number2));
+// UI logic
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
